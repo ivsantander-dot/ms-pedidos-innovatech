70985d4038585120926df72d6d198d90b5990d9f
ivsantander-dot <iv.santander@duocuc.cl>
2026-04-23 20:45:38 -0400

    inicializado el proyecto

---
+++ src/main/java/com/inovatech/ms_pedidos_innovatech/MsPedidosInnovatechApplication.java
@@ -0,0 +1,13 @@
+package com.inovatech.ms_pedidos_innovatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPedidosInnovatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPedidosInnovatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inovatech/ms_pedidos_innovatech/MsPedidosInnovatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovatech.ms_pedidos_innovatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosInnovatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
